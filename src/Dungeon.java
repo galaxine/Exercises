0171606d8af3c6c21e51f610907d167a18a6175e
soezdemir <oezdemir50@gmail.com>
2019-05-23 11:34:42 +0200

    created Dungeon exercise
    Start of the Dungeon exercise from Mooc.Fi.
    OOP part 2 week 10

---
+++ src/Dungeon.java
@@ -0,0 +1,14 @@
+public class Dungeon {
+private int length;
+private int height;
+private int vampires;
+private int moves;
+private boolean vampireMoves;
+    public Dungeon(int legnth, int height, int vampires, int moves, boolean vampiresMove) {
+
+    }
+    
+    public void run() {
+
+    }
+}
