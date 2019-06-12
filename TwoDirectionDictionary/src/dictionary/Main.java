f777bde720b07f5a83c322f2674262b32ddca9e5
Serdar Özdemir <oezdemir50@gmail.com>
2019-06-12 14:44:46 +0200

    finished up Exercise TwoDirectionDictionary

--- TwoDirectionDictionary/src/dictionary/Main.java
+++ TwoDirectionDictionary/src/dictionary/Main.java
@@ -1,14 +1,15 @@
 package dictionary;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        MindfulDictionary dict = new MindfulDictionary("src/words.txt");
-        dict.load();
-
-        System.out.println( dict.translate("apina"));
-        System.out.println(dict.translate("ohjelmointi"));
-        System.out.println(dict.translate("alla oleva"));
-
+        MindfulDictionary s = new MindfulDictionary("src/words.txt");
+        s.load();
+        s.translate("olut");
+        s.save();
     }
 }
--- TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
+++ TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
@@ -1,6 +1,6 @@
 package dictionary;
 
-import java.io.FileReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -8,28 +8,47 @@ import java.util.Scanner;
 public class MindfulDictionary {
     private HashMap<String, String> dict;
     private String file;
-    private FileReader read;
     public MindfulDictionary(String file) {
         this.dict = new HashMap<String, String>();
         this.file = file;
+    }
+
+    public MindfulDictionary() {
+        this.dict = new HashMap<String, String>();
 
     }
 
     public boolean load() {
-        Scanner fileReader = new Scanner(file);
-        if (!file.isEmpty()) {
+        try {
+            File file = new File (this.file);
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                String [] parts = line.split(":");
+                add(parts[0],parts[1]);
 
-            while (fileReader.hasNextLine()) {
-                String line = fileReader.nextLine();
-                String[] parts = line.split(":");
-                add(parts[0], parts[1]);
             }
+            scan.close();
             return true;
-        } else  {
+        } catch (FileNotFoundException e) {
             return false;
         }
     }
 
+    //
+    public boolean save() {
+        try {
+            FileWriter writer = new FileWriter(file);
+            for (String string :
+                    dict.keySet()) {
+                writer.write(string + ":" + dict.get(string));
+            }
+            writer.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
     // translation, if the same word is added, nothing happens.
     public void add(String word, String translation) {
         if (!dict.containsKey(word)) {
